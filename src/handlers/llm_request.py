0347a465bd327ee95538f6ba8237da4b99d5b562
egoric <egor-syrov@bk.ru>
2025-07-17 19:46:26 +0300

    fix echo handler

--- src/handlers/llm_request.py
+++ src/handlers/llm_request.py
@@ -1,9 +1,10 @@
 from aiogram import Router
 from aiogram.types import Message
+from aiogram.filters import Command
 
 router = Router()
 
 
-@router.message()
+@router.message(~Command())
 async def echo(message: Message):
     await message.answer(message.text)
